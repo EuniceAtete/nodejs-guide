ddc56d0c45b95dc801ec5754dd6a1c991c55e22c
Eunice Atete <euniceatete0@gmail.com>
2026-02-22 20:55:28 +0200

    Removing the listeners and all listeners

--- remLis.js
+++ remLis.js
@@ -6,11 +6,19 @@ function wishWkd(){
     console.log("You too!")
 }
 
+myEmitter.on('crime', (weapons) => {
+    console.log(`Put your ${weapons} down!`)
+})
 myEmitter.on('Have a great weekend',wishWkd);
 
 myEmitter.emit('Have a great weekend');
+
+myEmitter.emit('crime','guns');
 
 //Removing the listener
 myEmitter.removeListener('Have a great weekend',wishWkd);
 
 myEmitter.emit('Have a great weekend');//This will print nothing
+
+myEmitter.removeAllListeners('crime')// Removes only this listener
+myEmitter.removeAllListeners();// Removes all the listeners
