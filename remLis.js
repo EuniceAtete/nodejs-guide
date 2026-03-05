7f702609177543db49fa81d43feb4d0484b0be91
Eunice <euniceatete0@gmail.com>
2026-03-05 21:44:02 +0200

    Refactor event listener management in remLis.js

--- remLis.js
+++ remLis.js
@@ -2,23 +2,23 @@ const EventEmitter = require('events');
 
 const myEmitter = new EventEmitter();
 
-function wishWkd(){
-    console.log("You too!")
+function wishWkd() {
+    console.log("You too!");
 }
 
 myEmitter.on('crime', (weapons) => {
-    console.log(`Put your ${weapons} down!`)
-})
-myEmitter.on('Have a great weekend',wishWkd);
+    console.log(`Put your ${weapons} down!`);
+});
+
+myEmitter.on('Have a great weekend', wishWkd);
 
 myEmitter.emit('Have a great weekend');
+myEmitter.emit('crime', 'guns');
 
-myEmitter.emit('crime','guns');
+// Removing the listener
+myEmitter.removeListener('Have a great weekend', wishWkd);
 
-//Removing the listener
-myEmitter.removeListener('Have a great weekend',wishWkd);
+myEmitter.emit('Have a great weekend'); // This will print nothing
 
-myEmitter.emit('Have a great weekend');//This will print nothing
-
-myEmitter.removeAllListeners('crime')// Removes only this listener
-myEmitter.removeAllListeners();// Removes all the listeners
+myEmitter.removeAllListeners('crime'); // Removes only this listener
+myEmitter.removeAllListeners(); // Removes all listeners
