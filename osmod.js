f47239850703f70ba90235ff5c73620d88124fee
Eunice <euniceatete0@gmail.com>
2026-03-05 21:43:13 +0200

    Refactor OS module logging for clarity

--- osmod.js
+++ osmod.js
@@ -1,11 +1,23 @@
+// Import Node.js OS module
 const os = require('os');
 
-console.log(os.platform());
-console.log("Total memory: ",os.totalmem());
-console.log("Free memory: ",os.freemem());
-console.log("Home directory: ",os.homedir());
-console.log("Temporary directory: ",os.tmpdir());
-console.log("Uptime: ",os.uptime())
-console.log("OS Info: ",os.cpus());
-console.log("OS Type: ",os.type());
-console.log("OS Architecture: ",os.arch());
+// Display platform
+console.log("Platform:", os.platform());
+
+// Memory information
+console.log("Total memory:", os.totalmem());
+console.log("Free memory:", os.freemem());
+
+// Directory information
+console.log("Home directory:", os.homedir());
+console.log("Temporary directory:", os.tmpdir());
+
+// System uptime
+console.log("Uptime:", os.uptime(), "seconds");
+
+// CPU information
+console.log("CPU Info:", os.cpus());
+
+// OS details
+console.log("OS Type:", os.type());
+console.log("OS Architecture:", os.arch());
