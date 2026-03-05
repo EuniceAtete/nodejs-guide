62ef14d19220c15aa31f9d9b48b230c28f10cf50
Eunice <euniceatete0@gmail.com>
2026-03-05 21:40:09 +0200

    Update events.js

--- events.js
+++ events.js
@@ -6,4 +6,4 @@ myEmitter.on('borrow',(bookName) => {
     console.log(`You just borrowed ${bookName}`)
 });
 
-myEmitter.emit('borrow','The Boy With Stripped Pajamas');
+myEmitter.emit('borrow',' The Boy With Stripped Pajamas');
